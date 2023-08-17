af2238ad929dc786b678f12ba3ebbc39aee8bb8f
DADOU STEVEN <dgv1pro@gmail.com>
2023-08-17 22:13:30 +0200

    dadou

---
+++ src/main/java/appsinc/fr/lesmaths/FibonacciRecursiveNormal.java
@@ -0,0 +1,28 @@
+package appsinc.fr.lesmaths;
+
+public class FibonacciRecursiveNormal {
+
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    public String toString() {
+        for (int i = 0; i < 101; i++) {
+            System.out.println("fib(" + i + ") = " + fib(i));
+        }
+        return null;
+    }
+
+    public void fRN(String result) {
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        FibonacciRecursiveNormal fibonacciRecursiveNormal = new FibonacciRecursiveNormal();
+        fibonacciRecursiveNormal.fRN(fibonacciRecursiveNormal.toString());
+    }
+}
