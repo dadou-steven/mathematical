21babd45cdff148eccab2d860f5b21185ee2b128
DADOU STEVEN <dgv1pro@gmail.com>
2023-08-19 22:08:45 +0200

    dadou

--- src/main/java/appsinc/fr/lesmaths/SDeG.java
+++ src/main/java/appsinc/fr/lesmaths/SDeG.java
@@ -1,10 +1,5 @@
 package appsinc.fr.lesmaths;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-
 public class SDeG { // Séquence de grêlon.
 
     private int n;
